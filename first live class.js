b85602a42be59f7449e42bdfe4d9459d4f6f9806
Opeyemi Babarinde <65448867+Bishopxxx@users.noreply.github.com>
2022-09-06 21:21:06 +0100

    Add files via upload

---
+++ first live class.js
@@ -0,0 +1,76 @@
+
+/*let dog ='billy'
+ hello ='Rukky ${dog} '
+console.log(hello)
+
+//comment
+
+/* comment 
+
+let name ="ojo";
+let age =12; let time = "18:00";
+console.log(name,age,time)
+
+console.log(typeof hello)
+
+const person={
+    name: 'dan',
+    age: 12,
+    IsABoy: false,
+    }
+
+const a= true||false
+console.log(a)
+
+person['time'] = "6:00";
+person.value= null
+
+delete person.age;
+
+console.log(person.name)
+console.log(person.age)
+console.log(person)
+
+b =true&&true
+console.log(b)
+
+c =!true
+console.log(c)
+
+
+let age = 1;
+age++
+console.log(age)
+age--
+console.log(age)
+
+
+const sum = 2+2;
+const multiplication = 2*2;
+const division = 2/2;
+const minus = 2-2;
+const exponent = 2**2;
+yh
+console.log(sum,multiplication,division,minus,exponent)
+
+
+let age = 20;
+
+if (age < 18) {
+    console.log('valid')
+} else ( age > 24); {
+    console.log('yeah')
+}*/
+
+const phone = {
+    model: 'infinix',
+    battery: 'li-oh',
+    mah: 6000,
+    age:1,
+    IsAnAndroid: true,
+}
+
+phone.IsAnIphone = false
+
+alert(phone.model+' '+ 'is fucked')
+console.log(phone)
